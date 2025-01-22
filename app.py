3dc66e116f5864bff30fbd1f2dc5f77845606d9b
modar_algayal <mudar.algayal@helsinki.fi>
2025-01-22 11:23:45 +0200

    initialized Aiven and classes User and Project

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from openai import OpenAI
 
-load_dotenv()
+from backend.backend import Project, User, db, init_app
 
+load_dotenv()
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
 app = Flask(__name__)
+init_app(app)
 
 
 @app.route("/")
@@ -36,6 +38,59 @@ def quote_of_the_day():
     )
 
     return chat_completion.choices[0].message.content.strip()
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+
+    if not data.get("role") or not data.get("name") or not data.get("email"):
+        return jsonify({"error": "Missing required fields"}), 400
+
+    new_user = User(role=data["role"], name=data["name"], email=data["email"])
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return (
+        jsonify({"message": "User added successfully!", "user": new_user.to_dict}),
+        201,
+    )
+
+
+@app.route("/add_porject", methods=["POST"])
+def add_project():
+    data = request.get_json()
+
+    required_fields = [
+        "description",
+        "returned_x_value",
+        "returned_y_value",
+        "x_value_justification",
+        "y_value_justification",
+        "type",
+    ]
+
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Missing required fields"}), 400
+
+    new_project = Project(
+        description=data["description"],
+        returned_x_value=data["returned_x_value"],
+        returned_y_value=data["returned_y_value"],
+        x_value_justification=data["x_value_justification"],
+        y_value_justification=data["y_value_justification"],
+        type=data["type"],
+    )
+
+    db.session.add(new_project)
+    db.session.commit()
+    return (
+        jsonify(
+            {"message": "Project added successfully!", "project": new_project.to_dict()}
+        ),
+        201,
+    )
 
 
 if __name__ == "__main__":
