4f73eb5215875e9459025dd01eb0d4400327e41d
Germuu <jeremyyamajako@gmail.com>
2025-01-28 16:21:56 +0200

    Refactored repo

---
+++ services/openai_service.py
@@ -0,0 +1,18 @@
+import os
+import json
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+with open("prompt.txt", "r") as file:
+    instruction_prompt = file.read().strip()
+
+
+def get_openai_completion(description):
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": instruction_prompt},
+            {"role": "user", "content": description},
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return json.loads(response.choices[0].message.content)
